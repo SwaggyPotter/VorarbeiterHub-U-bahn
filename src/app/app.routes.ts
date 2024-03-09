a7709daf017b92f48334804e5722376aad8b1e67
Tim <tim.spiele1@freenet.de>
2024-03-09 06:27:05 +0100

    Implement Login,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'main', component: AppComponent },
+];
+
+
+@NgModule({
+    imports: [
+        AppComponent,
+        LoginComponent
+    ],
+    declarations: []
+})
+export class AppRoutingModule { }
